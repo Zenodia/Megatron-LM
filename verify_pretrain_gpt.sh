44780bb95549af5c6ea39e8793c2229dcea4095e
zenodia <zenodia@gmail.com>
2021-03-26 16:06:44 +0100

    adding gpt preprocess+trainig bash scripts

---
+++ verify_pretrain_gpt.sh
@@ -0,0 +1,33 @@
+CHECKPOINT_PATH=./zh_ckpt
+VOCAB_FILE=/workspace/cn/bpe/32k/vocab.json
+MERGE_FILE=/workspace/cn/bpe/32k/merges.txt
+DATA_PATH=/workspace/dataset/demo_gpt_sentence_text_sentence
+
+GPT_ARGS="--num-layers 24 \
+          --hidden-size 1024 \
+          --num-attention-heads 16 \
+          --seq-length 1024 \
+          --max-position-embeddings 1024 \
+          --micro-batch-size 4 \
+          --global-batch-size 8 \
+          --lr 0.00015 \
+          --train-iters 500000 \
+          --lr-decay-iters 320000 \
+          --lr-decay-style cosine \
+          --vocab-file $VOCAB_FILE \
+          --merge-file $MERGE_FILE \
+          --lr-warmup-fraction .01 \
+          --fp16"
+
+OUTPUT_ARGS="--log-interval 10 \
+             --save-interval 500 \
+             --eval-interval 100 \
+             --eval-iters 10 \
+             --checkpoint-activations"
+
+python pretrain_gpt.py \
+       $GPT_ARGS \
+       $OUTPUT_ARGS \
+       --save $CHECKPOINT_PATH \
+       --load $CHECKPOINT_PATH \
+       --data-path $DATA_PATH \
